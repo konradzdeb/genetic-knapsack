5c7df040176a53b1b8757f861b8b303c6ba24835
Konrad Zdeb <konrad.zdeb@me.com>
2022-02-14 16:37:27 +0100

    Further development
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

--- solution/knapsack_ga_solver.py
+++ solution/knapsack_ga_solver.py
@@ -12,6 +12,7 @@ from operator import le
 import random
 from typing import Final
 import statistics
+from math import floor
 
 
 # Classes
@@ -27,6 +28,7 @@ class Knapsack:
         default_factory=lambda: [[]], repr=False)
     optimal_value: float = 0    # Optimal utility value
     optimal_weight: float = 0   # Optimal weight value
+    capacity: Final[int] = 0
 
     def solution_weight(self, iteration):
         """Provide a weight information for a specific solution"""
@@ -40,7 +42,8 @@ class Knapsack:
 
     def solutions_rank(self):
         """Provide a utility rank of all solutions"""
-        solution_values = [self.solution_value(x) for x in range(len(self.population))]
+        solution_values = [self.solution_value(
+            x) for x in range(len(self.population))]
         return sorted(range(len(solution_values)), key=solution_values.__getitem__)
 
     def __repr__(self) -> str:
@@ -62,7 +65,7 @@ def generate_random_solution(items, capacity):
         if current_weight + random_item[0] <= capacity:
             # Add item to solution
             random_solution.append(random_item)
-            current_weight += random_item[1]
+            current_weight += random_item[0]
     # Returned packed knapsack
     return random_solution
 
@@ -90,7 +93,7 @@ def calculate_utility(solution):
 
 def selection(knapsack_object, fitness_rank=None):
     """Select objects from Knapsack that are of defined fitness
-    
+
     If fitness is provided, objects above the threshold are selected for situation
     where fitness is not provided the top half of objects is selected. Fitness 
     corresponds to minimum rank value.
@@ -106,24 +109,25 @@ def selection(knapsack_object, fitness_rank=None):
         threshold = statistics.median(knapsack_object.solutions_rank())
     else:
         threshold = fitness_rank
-    index_selected = [x for x in range(len(solutions_rank)) if solutions_rank[x] < threshold]
+    index_selected = [x for x in range(
+        len(solutions_rank)) if solutions_rank[x] < threshold]
     return index_selected
 
 
-def mutation(knapsack_solution, items, pop_item=None):
+def mutation(knapsack_solution, items, pop_item=None, attempts=1000):
     """Mutate object from knapsack
 
     Select object from Knapsack population and mutate.
-    
+
     Args:
         knapsack_object (Knapsack): Object of a Knapsack
+        items ([[int, int]]): List of items available to take to mutation
+        pop_item (int, optional): Index of object to mutate. Defaults to None.
     """
     # Select object from population
     if pop_item is None:
-        pop_solution = random.sample(knapsack_solution.population, 1)[0]
-    else:
-        # TODO: Check if this needs unnesting
-        pop_solution = knapsack_solution.population[pop_item]
+        pop_item = random.randint(0, len(knapsack_solution.population) - 1)
+    pop_solution = knapsack_solution.population[pop_item]
     # Randomly mutate an object by replacing one item with the item from the
     # available items. The mutation can produce adverse effect (utility decreases)
     # or beneficial effect (utility increases). Try limited number of items and
@@ -132,7 +136,7 @@ def mutation(knapsack_solution, items, pop_item=None):
     remaining_items = [x for x in items if x not in pop_solution]
     # Get population weight
     pop_weight = knapsack_solution.solution_weight(pop_item)
-    for _ in range(10):
+    for _ in range(attempts):
         # Select random item from available items
         random_new_item = random.sample(remaining_items, 1)[0]
         random_sol_item = random.sample(pop_solution, 1)[0]
@@ -145,6 +149,57 @@ def mutation(knapsack_solution, items, pop_item=None):
     return pop_solution
 
 
+def crossover(knapsack_object, pop_item1=None, pop_item2=None, rate=0.5):
+    """Cross over from two objects
+
+    Take two objects and cross over the items across them.
+
+    Args:
+        knapsack_object (Knapsack): Knapsack object with available solutions
+        pop_item1 (_type_, int): Index of a first solution. Defaults to None.
+        pop_item2 (_type_, int): Index of a second solution. Defaults to None.
+
+    Returns:
+        [int]: A solution with cross over from the selected items
+    """
+    if pop_item1 is None:
+        pop_item1 = random.randint(0, len(knapsack_object.population) - 1)
+    if pop_item2 is None:
+        # Select another on random and ensure that it's not the same
+        available_solutions = range(0, len(knapsack_object.population))
+        available_solutions.remove(pop_item1)
+        pop_item2 = random.sample(available_solutions, 1)[0]
+    # Shuffle items from across two solutions
+    pop_solution1 = knapsack_object.population[pop_item1]
+    pop_solution2 = knapsack_object.population[pop_item2]
+    pop_items_1 = random.sample(
+        pop_solution1, floor(len(pop_solution2) * rate))
+    for ele in pop_items_1:
+        pop_solution1.remove(ele)
+    # Add random selection of items from the other
+    pop_items_2 = random.sample(pop_solution2, len(pop_items_1))
+    for ele in pop_items_2:
+        pop_solution1.append(ele)
+    return pop_solution1
+
+
+def genetic_algorithm(available_items, knapsack_object, iterations=1000):
+    """Run genetic algorithm"""
+    for _ in range(iterations):
+        # Evaluate solutions in population
+        index_optimal_items = selection(knapsack_object)
+        # Mutate solutions
+        for solution_index in index_optimal_items:
+            # Mutate solution
+            knapsack_object.population[solution_index] = mutation(
+                knapsack_object, available_items, pop_item=solution_index)
+        # Cross over solutions
+        pop_item_1 = random.sample(index_optimal_items, 1)
+        pop_item_2 = random.sample(index_optimal_items, 1)
+        knapsack_object.population[pop_item_1] = crossover(
+                knapsack_object, pop_item_1, pop_item_2)
+
+
 # Tests
 random.seed(123)
 # Generate some data for tests, first is utility, second is weight
@@ -155,9 +210,8 @@ CAPCITY: Final[int] = 30
 POPULATION_SIZE: Final[int] = 1000
 # Instantiate Knapsack class
 genetic_knapsack = Knapsack(population=generate_population(items=items_w_weights, population_size=POPULATION_SIZE,
-                                                           capacity=CAPCITY, solution_generator=generate_random_solution))
+                                                           capacity=CAPCITY, solution_generator=generate_random_solution), capacity=CAPCITY)
 desired_fitness = 10
 res_selection = selection(genetic_knapsack, fitness_rank=desired_fitness)
-res_selection
 
 # TODO: Implement gentic algorithm functions
--- solution/test_knapsack_ga_solver.py
+++ solution/test_knapsack_ga_solver.py
@@ -63,9 +63,56 @@ class TestUtilityCalculations(TestGeneratingRandomSolution):
     def setUp(self):
         return super().setUp()
 
-    def test_function_output(self):
+    def test_utility_calculation(self):
         """Test if the function returns a list of solutions"""
         # Calculate utility
         utility = calculate_utility(self.solution)
         self.assertIsInstance(utility, int)
         self.assertGreater(utility, 100)
+
+
+class TestGeneratingKnapsackObject(TestGeneratingRandomSolution):
+
+    def setUp(self):
+        super(TestGeneratingKnapsackObject, self).setUp()
+        # Create sample knapsack object
+        self.knapsack = Knapsack(population=generate_population(items=self.items_weight_value, population_size=1000,
+                                                                capacity=self.CAPACITY, solution_generator=generate_random_solution))
+
+    def test_knapsack_class(self):
+        self.assertIsInstance(self.knapsack, Knapsack)
+
+
+class TestGeneticFunctions(TestGeneratingKnapsackObject):
+
+    def setUp(self):
+        super(TestGeneticFunctions, self).setUp()
+
+    def test_selection(self):
+        """Test if the selection function returns a list of solutions"""
+        # Select solutions
+        selected_solutions = selection(self.knapsack, fitness_rank=None)
+        # Check if the selected solutions is a list
+        self.assertIsInstance(selected_solutions, list)
+        # Check if the selected solutions is not empty
+        self.assertTrue(len(selected_solutions) > 0)
+        # Check if the selected solutions is a list of lists
+        for solution in selected_solutions:
+            self.assertIsInstance(solution, int)
+        # Check if the selected solutions is a list of lists
+        for solution in selected_solutions:
+            self.assertIsNotNone(solution)
+
+    def test_mutation(self):
+        """Test if mutation returns a solution"""
+        mutated_solution = mutation(knapsack_solution=self.knapsack, items=self.items_weight_value)
+        self.assertIsInstance(mutated_solution, list)
+        self.assertGreaterEqual(len(mutated_solution), 1)       
+
+    def test_crossover(self):
+        """Test if crossover returns a solution"""
+        crossover_solution = crossover(self.knapsack, 1, 2, 0.5)
+        self.assertIsInstance(crossover_solution, list)
+        self.assertGreaterEqual(len(crossover_solution), 1)
+
+
